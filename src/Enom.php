2626df23ea68ecb0007736ab39171d01dc74dd3c
Radu Boncea <radu@rotld.ro>
2017-03-28 20:07:46 +0300

    fixed issue with hard-coded enom api base url

--- src/Enom.php
+++ src/Enom.php
@@ -9,10 +9,10 @@ class Enom
 
     protected $client;
 
-    public function __construct($userId, $password)
+    public function __construct($userId, $password, $base_url)
     {
         $this->client = new Client([
-            'base_url' => 'https://resellertest.enom.com/interface.asp',
+            'base_url' => $base_url,
             'defaults' => [
                 "query" => [
                     'uid'          => $userId,
