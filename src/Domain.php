ce138e5f696d30e8feda279b853274b04164b6e6
Alex Gheorghita <gheorghitacristian@mac.com>
2018-05-13 21:17:47 +0300

    Added Update Expired Domains command

--- src/Domain.php
+++ src/Domain.php
@@ -182,6 +182,22 @@ class Domain
         return $response;
     }
 
+    public function updateExpiredDomains($domain, $period)
+    {
+        $response = $this->doGetRequest('UpdateExpiredDomains', [
+            'DomainName' => $domain,
+            'NumYears' => $period
+        ]);
+
+        $response = $this->parseXMLObject($response);
+
+        if ($response->ErrCount > 0) {
+            throw new EnomApiException($response->errors);
+        }
+
+        return $response;
+    }
+
     public function getStatus($sld, $tld, $orderId)
     {
         $response = $this->doGetRequest('GetDomainStatus', [
